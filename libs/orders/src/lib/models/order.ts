ac65ce817e0931b87aa62dd041f81b47f901b29d
lashagelashvili <las.gelashvili@gmail.com>
2023-06-02 13:13:16 +0400

    order model

---
+++ libs/orders/src/lib/models/order.ts
@@ -0,0 +1,17 @@
+import { User } from '@bluebit/users';
+import { OrderItem } from './order-item';
+
+export class Order {
+  id?: string;
+  orderItem?: OrderItem;
+  shippingAddress1?: string;
+  shippingAddress2?: string;
+  city?: string;
+  zip?: string;
+  country?: string;
+  phone?: string;
+  status?: string;
+  totalPrice?: string;
+  user?: User;
+  dateOrdered?: string;
+}
