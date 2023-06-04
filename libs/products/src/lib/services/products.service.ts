f436b4a4494ff5b2f9a1e5c08f61d77fd9e61066
lashagelashvili <las.gelashvili@gmail.com>
2023-06-04 23:07:26 +0400

    add products service

---
+++ libs/products/src/lib/services/products.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@env/environment';
+import { Product } from '../models/product';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductsService {
+  apiURLProducts = environment.apiURL + 'products';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.apiURLProducts);
+  }
+
+  getProduct(productId: string): Observable<Product> {
+    return this.http.get<Product>(`${this.apiURLProducts}/${productId}`);
+  }
+
+  createProduct(productData: FormData): Observable<Product> {
+    return this.http.post<Product>(this.apiURLProducts, productData);
+  }
+
+  updateProduct(productData: FormData, productId: string): Observable<Product> {
+    return this.http.put<Product>(
+      `${this.apiURLProducts}/${productId}`,
+      productData
+    );
+  }
+
+  deleteProduct(productId: string): Observable<object> {
+    return this.http.delete<object>(`${this.apiURLProducts}/${productId}`);
+  }
+}
