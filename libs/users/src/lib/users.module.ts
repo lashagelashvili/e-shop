b0a468411306beffdaa5504a06806fa6745074fd
lashagelashvili <las.gelashvili@gmail.com>
2023-06-06 15:23:29 +0400

    setup usersmodule

--- libs/users/src/lib/users.module.ts
+++ libs/users/src/lib/users.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule, Route } from '@angular/router';
+import { RouterModule } from '@angular/router';
 import { usersRoutes } from './lib.routes';
+import { LoginComponent } from './pages/login/login.component';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(usersRoutes), RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(usersRoutes),
+    RouterModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  declarations: [LoginComponent],
 })
 export class UsersModule {}
