51cef191291dbe98425fc6ea38cde9a9f3ba8579
lashagelashvili <las.gelashvili@gmail.com>
2023-06-06 15:21:53 +0400

    update dashboard component

--- apps/admin/src/app/pages/dashboard/dashboard.component.ts
+++ apps/admin/src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '@bluebit/orders';
+import { ProductsService } from '@bluebit/products';
+import { UsersService } from '@bluebit/users';
+import { combineLatest } from 'rxjs';
 
 @Component({
   selector: 'admin-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  statistics = [];
 
+  constructor(
+    private ordersService: OrdersService,
+    private productService: ProductsService,
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    combineLatest([
+      this.ordersService.getOrderCount(),
+      this.productService.getProductCount(),
+      this.usersService.getUserCount(),
+      this.ordersService.getTotalSales(),
+    ]).subscribe((orders) => (this.statistics = orders));
+  }
 }
