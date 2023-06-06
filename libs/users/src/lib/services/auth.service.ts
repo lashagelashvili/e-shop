eb497e6acefa5fde2ed9a8ed82f0a863a92186b0
lashagelashvili <las.gelashvili@gmail.com>
2023-06-06 15:23:22 +0400

    authservice

---
+++ libs/users/src/lib/services/auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { LocalstorageService } from './localstorage.service';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  apiURLAuth = environment.apiURL + 'users';
+
+  constructor(
+    private http: HttpClient,
+    private localstorageService: LocalstorageService,
+    private router: Router
+  ) {}
+
+  login(email: string, password: string): Observable<User> {
+    return this.http.post<User>(`${this.apiURLAuth}/login`, {
+      email,
+      password,
+    });
+  }
+
+  logout() {
+    this.localstorageService.removeToken();
+    this.router.navigate(['/login']);
+  }
+}
