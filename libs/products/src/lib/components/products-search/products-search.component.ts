f954534f4635f2fd2ec924f0f0bdd2b5f48a9893
lashagelashvili <las.gelashvili@gmail.com>
2023-06-07 11:31:24 +0400

    add products search component

---
+++ libs/products/src/lib/components/products-search/products-search.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'products-search',
+  templateUrl: './products-search.component.html',
+  styles: [],
+})
+export class ProductsSearchComponent {}
