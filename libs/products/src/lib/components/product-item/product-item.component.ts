2b7d5b1a3a3ebf5dccf9ca7a34dad980786245e9
lashagelashvili <las.gelashvili@gmail.com>
2023-06-22 19:21:30 +0400

    add product to cart on click

--- libs/products/src/lib/components/product-item/product-item.component.ts
+++ libs/products/src/lib/components/product-item/product-item.component.ts
@@ -1,11 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/product';
+import { CartItem, CartService } from '@bluebit/orders';
 
 @Component({
-  selector: 'products-product-item',
+  selector: 'bluebit-products-product-item',
   templateUrl: './product-item.component.html',
   styles: [],
 })
 export class ProductItemComponent {
-  @Input() product: Product;
+  @Input() product!: Product;
+
+  constructor(private cartService: CartService) {}
+
+  addProductToCart() {
+    const cartItem: CartItem = {
+      productId: this.product.id,
+      quantity: 1,
+    };
+    this.cartService.setCartItem(cartItem);
+  }
 }
